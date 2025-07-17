aba1288bb75a3136b4e26c505805f125bee4e3d0
Tom Wolfe <trwolfe13@gmail.com>
2025-07-17 20:29:47 +0100

    docs: add documentation

--- src/Hamelin/PipelineApplicationOptions.cs
+++ src/Hamelin/PipelineApplicationOptions.cs
@@ -1,9 +1,27 @@
 namespace Hamelin;
 
+/// <summary>
+/// Settings for constructing a <see cref="PipelineApplicationBuilder"/>.
+/// </summary>
 public class PipelineApplicationOptions
 {
+    /// <summary>
+    /// Gets or sets the command-line arguments to add to the <see cref="PipelineApplicationBuilder.Configuration"/>.
+    /// </summary>
     public string[]? Args { get; init; }
+
+    /// <summary>
+    /// Gets or sets the environment name.
+    /// </summary>
     public string? EnvironmentName { get; init; }
+
+    /// <summary>
+    /// Gets or sets the application name.
+    /// </summary>
     public string? ApplicationName { get; init; }
+
+    /// <summary>
+    /// Gets or sets the content root path.
+    /// </summary>
     public string? ContentRootPath { get; init; }
 }
