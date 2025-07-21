fdaf471fd057fcdd3cb88994a44c45ef2788d96e
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-07-21 13:54:14 +0100

    chore: fix namespaces

--- tests/Hamelin.Tests.Unit/Internal/PipelineHostTests.cs
+++ tests/Hamelin.Tests.Unit/Internal/PipelineHostTests.cs
@@ -4,7 +4,7 @@ using Hamelin.Tests.Unit.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Hamelin.Tests.Unit;
+namespace Hamelin.Tests.Unit.Internal;
 
 public class PipelineHostTests
 {
