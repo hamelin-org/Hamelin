9668d2086c19e04e83d1bafc33abee62eedb4e1a
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-07-21 15:27:18 +0100

    feat: add keyed state

--- src/Hamelin/Internal/DefaultPipelineState.cs
+++ src/Hamelin/Internal/DefaultPipelineState.cs
@@ -2,19 +2,20 @@ namespace Hamelin.Internal;
 
 internal class DefaultPipelineState : IPipelineState
 {
-    private readonly Dictionary<Type, object> _state = new();
+    private readonly Dictionary<(Type, string), object> _state = new();
 
-    public T? Get<T>()
+    public T? Get<T>(string? key = null)
     {
-        if (_state.TryGetValue(typeof(T), out object? value))
+        if (_state.TryGetValue((typeof(T), key ?? ""), out object? value))
         {
             return (T)value;
         }
+
         return default;
     }
 
-    public void Set<T>(T value) where T : notnull
+    public void Set<T>(T value, string? key = null) where T : notnull
     {
-        _state[typeof(T)] = value;
+        _state[(typeof(T), key ?? "")] = value;
     }
 }
