c696f33244d18c2eaa5af451662ea09722d2f3c4
Tom Wolfe <10941385+tom-wolfe@users.noreply.github.com>
2025-07-22 09:55:32 +0100

    Add current working directory to context (#12)

--- src/Hamelin/Internal/DefaultPipelineContext.cs
+++ src/Hamelin/Internal/DefaultPipelineContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 
 namespace Hamelin.Internal;
 
-internal class DefaultPipelineContext(IFileProvider fileSystem, IPipelineState state) : IPipelineContext
+internal class DefaultPipelineContext(IFileProvider fileSystem, IPipelineState state, IHostEnvironment env) : IPipelineContext
 {
     public IFileProvider FileSystem { get; } = fileSystem;
     public IPipelineState State { get; } = state;
+    public string CurrentDirectory { get; } = env.ContentRootPath;
 }
