5279eac5ec29f5efe6bfccf9e16dadbb23cdc519
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-07-23 16:47:39 +0100

    feat: add Clean step.

--- build/Hamelin.Build/Program.cs
+++ build/Hamelin.Build/Program.cs
@@ -1,8 +1,21 @@
 ﻿using Hamelin;
+using Hamelin.Build;
+using Hamelin.Build.Steps;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = PipelineApplication.CreateBuilder(args);
 
+builder.Services.AddStepsFromAssemblyContaining<Program>();
+
+builder.Services.AddOptions<BuildOptions>()
+    .BindConfiguration("Build")
+    .Validate(b => !string.IsNullOrEmpty(b.ArtifactsDirectory))
+    .Validate(b => !string.IsNullOrEmpty(b.TempDirectory))
+    .ValidateOnStart();
+
 var pipeline = builder.Build();
+
+pipeline.UseStep<CleanStep>();
 
 pipeline.Run();
