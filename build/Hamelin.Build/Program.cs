2430fa6e3e39b4fb861668691888ee3346d5493d
Tom Wolfe <10941385+tom-wolfe@users.noreply.github.com>
2025-07-28 10:04:07 +0100

    Add Format Step (#20)
    Adds a formatting step to the bootstrap builder. Here's an example
    output:
    
    ```
    info: Hamelin.Build.Steps.CleanStep[0]
          Cleaning temp and artifact directories.
    info: Hamelin.Build.Steps.FormatStep[0]
          Running command: dotnet format --verify-no-changes
    info: Hamelin.Build.Steps.FormatStep[0]
          Process started; ID: 7920
    fail: Hamelin.Build.Steps.FormatStep[0]
          /Users/tomwolfe/Development/Hamelin/src/Hamelin/IPipelineContext.cs(8,34): error WHITESPACE: Fix whitespace formatting. Replace 1 characters with '\n'. [/Users/tomwolfe/Development/Hamelin/src/Hamelin/Hamelin.csproj]
    info: Hamelin.Build.Steps.FormatStep[0]
          Process exited; Code: 2
    ```

--- build/Hamelin.Build/Program.cs
+++ build/Hamelin.Build/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Hamelin;
 using Hamelin.Build;
+using Hamelin.Build.Services;
 using Hamelin.Build.Steps;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = PipelineApplication.CreateBuilder(args);
 
-builder.Services.AddStepsFromAssemblyContaining<Program>();
+builder.Services
+    .AddScoped<ICommandRunner, CliWrapCommandRunner>()
+    .AddStepsFromAssemblyContaining<Program>();
 
 builder.Services.AddOptions<BuildOptions>()
     .BindConfiguration("Build")
@@ -16,6 +19,8 @@ builder.Services.AddOptions<BuildOptions>()
 
 var pipeline = builder.Build();
 
-pipeline.UseStep<CleanStep>();
+pipeline
+    .UseStep<CleanStep>()
+    .UseStep<FormatStep>();
 
 pipeline.Run();
