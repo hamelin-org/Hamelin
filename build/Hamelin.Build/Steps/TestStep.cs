73132b5cf22660323598b532474a9cb6e898aa19
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-07-28 10:53:47 +0100

    fix: run tests with same config as build

--- build/Hamelin.Build/Steps/TestStep.cs
+++ build/Hamelin.Build/Steps/TestStep.cs
@@ -1,14 +1,15 @@
 using Hamelin.Build.Services;
+using Microsoft.Extensions.Options;
 
 namespace Hamelin.Build.Steps;
 
-public class TestStep(ICommandRunner commands) : IPipelineStep
+public class TestStep(IOptions<BuildOptions> options, ICommandRunner commands) : IPipelineStep
 {
     public async Task Run(CancellationToken cancellationToken = default)
     {
         await commands.Run(
             command: "dotnet",
-            arguments: ["test", "--no-build"],
+            arguments: ["test", "--no-build", "--configuration", options.Value.Configuration],
             cancellationToken
         );
     }
