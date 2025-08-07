07a5b18fefa3f73f81b7b1c4653c47653e9c77f9
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-07 12:23:31 +0100

    test: fix tests

---
+++ tests/Hamelin.Tests.Unit/Steps/PipelineStepCollectionTests.cs
@@ -0,0 +1,31 @@
+using Hamelin.Steps;
+
+namespace Hamelin.Tests.Unit.Steps;
+
+public class PipelineStepCollectionTests
+{
+    [Fact]
+    public void GetSteps_WithSteps_ReturnsCorrectSteps()
+    {
+        // Arrange
+        var collection = new PipelineStepCollection();
+        collection.AddStep<DummyStep1>();
+        collection.AddStep<DummyStep2>();
+
+        // Act
+        var steps = collection.GetSteps();
+
+        // Assert
+        steps.ShouldBeEquivalentTo(new List<Type>() { typeof(DummyStep1), typeof(DummyStep2) });
+    }
+
+    private class DummyStep1 : IPipelineStep
+    {
+        public Task Run(CancellationToken cancellationToken = default) => Task.CompletedTask;
+    }
+
+    private class DummyStep2 : IPipelineStep
+    {
+        public Task Run(CancellationToken cancellationToken = default) => Task.CompletedTask;
+    }
+}
