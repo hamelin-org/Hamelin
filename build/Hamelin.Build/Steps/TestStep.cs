f550cc1c95efd4d4044d7c74f53b63bd9c4b0293
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-14 11:23:25 +0100

    fix: remove redundant summary

--- build/Hamelin.Build/Steps/TestStep.cs
+++ build/Hamelin.Build/Steps/TestStep.cs
@@ -1,14 +1,9 @@
 using Hamelin.Build.Services;
-using Hamelin.Runtimes.GitHubActions;
 using Microsoft.Extensions.Options;
 
 namespace Hamelin.Build.Steps;
 
-public class TestStep(
-    IOptions<BuildOptions> options,
-    ICommandRunner commands,
-    IGitHubActionsCommands gha
-) : IPipelineStep
+public class TestStep(IOptions<BuildOptions> options, ICommandRunner commands) : IPipelineStep
 {
     public async Task Run(CancellationToken cancellationToken = default)
     {
@@ -16,18 +11,6 @@ public class TestStep(
             command: "dotnet",
             arguments: ["test", "--no-build", "--configuration", options.Value.Configuration],
             cancellationToken
-        );
-
-        string summary = """
-                         ## Build Summary
-
-                         Build completed successfully.
-                         """;
-
-        await File.WriteAllTextAsync(
-            path: Environment.GetEnvironmentVariable("GITHUB_STEP_SUMMARY")!,
-            contents: summary,
-            cancellationToken: cancellationToken
         );
     }
 }
