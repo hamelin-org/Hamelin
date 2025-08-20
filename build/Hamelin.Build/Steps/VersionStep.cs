cbd535680a2701ac961c823de8e02d9777ad65e5
Tom Wolfe <10941385+tom-wolfe@users.noreply.github.com>
2025-08-20 15:22:09 +0100

    Validate version increment (#39)
    Adds a build check that enforces NuGet package versions only go up by
    one each time, and that any other version parts reset to 0.

--- build/Hamelin.Build/Steps/VersionStep.cs
+++ build/Hamelin.Build/Steps/VersionStep.cs
@@ -10,7 +10,7 @@ using NuGet.Versioning;
 namespace Hamelin.Build.Steps;
 
 public class VersionStep(
-    ILoggerFactory loggerFactory,
+    ILogger<VersionStep> logger,
     IOptions<BuildOptions> options,
     IPipelineContext context
 ) : IPipelineStep
@@ -27,17 +27,66 @@ public class VersionStep(
         var packageSource = new PackageSource(options.Value.NuGetFeed) { Credentials = credentials };
         SourceRepository repository = Repository.Factory.GetCoreV3(packageSource);
         var resource = await repository.GetResourceAsync<FindPackageByIdResource>(cancellationToken);
-        IEnumerable<NuGetVersion> versions = await resource.GetAllVersionsAsync(
+        var versions = (await resource.GetAllVersionsAsync(
             projectInfo.Name,
             new SourceCacheContext(),
-            new NuGetLoggerAdapter(loggerFactory.CreateLogger<FindPackageByIdResource>()),
+            new NuGetLoggerAdapter(logger),
             cancellationToken
-        );
+        )).ToList();
+
+        if (versions.Count == 0)
+        {
+            logger.LogInformation("No existing versions found for package {PackageName}.", projectInfo.Name);
+            if (projectInfo.Version != NuGetVersion.Parse("0.0.1"))
+            {
+                throw new Exception(
+                    $"No existing versions found for package {projectInfo.Name}. The first version must be 0.0.1.");
+            }
+        }
 
         NuGetVersion? match = versions.FirstOrDefault(c => c == projectInfo.Version);
         if (match != null)
         {
             throw new Exception("Package version already exists.");
         }
+
+        var latestVersion = versions.Max(v => v) ?? versions.Last();
+        Queue<int> versionParts = new([projectInfo.Version.Major, projectInfo.Version.Minor, projectInfo.Version.Patch, projectInfo.Version.Revision]);
+        Queue<int> latestVersionParts = new([latestVersion.Major, latestVersion.Minor, latestVersion.Patch, latestVersion.Revision]);
+
+        while (versionParts.Count != 0)
+        {
+            int part = versionParts.Dequeue();
+            int latestPart = latestVersionParts.Dequeue();
+
+            if (part < latestPart)
+            {
+                throw new Exception(
+                    $"Project version {projectInfo.Version} is not greater than the latest version {latestVersion}.");
+            }
+
+            if (part > latestPart + 1)
+            {
+                throw new Exception(
+                    $"Project version {projectInfo.Version} is not a valid increment of the latest version {latestVersion}.");
+            }
+
+            if (part == latestPart + 1)
+            {
+                // If we are incrementing the version, we can stop checking further parts
+                break;
+            }
+        }
+
+        while (versionParts.Count != 0)
+        {
+            int part = versionParts.Dequeue();
+            if (part != 0)
+            {
+                throw new Exception("When incrementing a version number, all remaining version parts must be 0.");
+            }
+        }
+
+        logger.LogInformation("Version {Version} is valid and can be used for package {PackageName}.", projectInfo.Version, projectInfo.Name);
     }
 }
