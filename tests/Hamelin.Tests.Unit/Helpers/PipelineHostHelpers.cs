c141d7996b2819e582a4aeb01ff8f3c5419995be
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-21 10:34:56 +0100

    test: add tests

--- tests/Hamelin.Tests.Unit/Helpers/PipelineHostHelpers.cs
+++ tests/Hamelin.Tests.Unit/Helpers/PipelineHostHelpers.cs
@@ -1,3 +1,4 @@
+using Hamelin.Hooks;
 using Hamelin.Internal;
 using Hamelin.Steps;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,10 +8,12 @@ using Microsoft.Extensions.Options;
 
 namespace Hamelin.Tests.Unit.Helpers;
 
-internal class PipelineHostHelpers
+internal static class PipelineHostHelpers
 {
     public static PipelineHost CreateHost(
         IPipelineStep[]? steps = null,
+        IPrePipelineHook[]? prePipelineHooks = null,
+        IPostPipelineHook[]? postPipelineHooks = null,
         Action<PipelineExecutionOptions>? configure = null,
         IPipelineContext? context = null,
         IHostApplicationLifetime? lifetime = null,
@@ -21,15 +24,24 @@ internal class PipelineHostHelpers
         context ??= Substitute.For<IPipelineContext>();
         lifetime ??= Substitute.For<IHostApplicationLifetime>();
 
-        var provider = Substitute.For<IPipelineStepProvider>();
-        provider.GetSteps().Returns(steps ?? []);
+        var stepProvider = Substitute.For<IPipelineStepProvider>();
+        stepProvider.GetSteps().Returns(steps ?? []);
 
         var services = new ServiceCollection()
-            .AddSingleton(provider)
-            .AddSingleton(context)
-            .BuildServiceProvider();
+            .AddSingleton(stepProvider)
+            .AddSingleton(context);
 
-        var scopeFactory = ServiceScopeHelpers.CreateScopeFactory(services);
+        foreach (var hook in prePipelineHooks ?? [])
+        {
+            services.AddScoped<IPrePipelineHook>(_ => hook);
+        }
+        foreach (var hook in postPipelineHooks ?? [])
+        {
+            services.AddScoped<IPostPipelineHook>(_ => hook);
+        }
+
+        var provider = services.BuildServiceProvider();
+        var scopeFactory = ServiceScopeHelpers.CreateScopeFactory(provider);
 
         PipelineExecutionOptions pipelineExecutionOptions = new();
         configure?.Invoke(pipelineExecutionOptions);
