ea012af7bb081040dddc3d044fe8aab209caa8cb
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-26 16:14:08 +0100

    refactor: remove unnecessary code

--- src/Hamelin/Internal/DefaultPipelineStepRunner.cs
+++ src/Hamelin/Internal/DefaultPipelineStepRunner.cs
@@ -1,4 +1,3 @@
-using Hamelin.Hooks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,44 +19,10 @@ internal class DefaultPipelineStepRunner(
             return StepExecutionSummary.FromStep(step, PipelineStepResult.StoppedAfterCancel);
         }
 
-        await RunPreStepHooks(scope);
-
         var result = await RunStepCore(step, cancellationToken);
         var summary = StepExecutionSummary.FromStep(step, result);
 
-        await RunPostStepHooks(scope, summary);
-
         return summary;
-    }
-
-    private async Task RunPreStepHooks(AsyncServiceScope scope)
-    {
-        var hooks = scope.ServiceProvider.GetServices<IPreStepHook>().ToList();
-        if (hooks.Count == 0)
-        {
-            logger.LogDebug("No pre-step hooks registered.");
-            return;
-        }
-
-        logger.LogDebug("Running pre-step hooks...");
-        foreach (var hook in hooks)
-        {
-            await RunPreStepHook(hook);
-        }
-
-        logger.LogDebug("Pre-step hooks complete.");
-    }
-
-    private async Task RunPreStepHook(IPreStepHook hook)
-    {
-        try
-        {
-            await hook.PreStep(CancellationToken.None);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Unhandled error during pre-step hook. Continuing...");
-        }
     }
 
     private async Task<PipelineStepResult> RunStepCore(IPipelineStep step, CancellationToken cancellationToken)
@@ -81,36 +46,6 @@ internal class DefaultPipelineStepRunner(
                     lifetime.StopApplication();
                     return PipelineStepResult.StoppedOnError(ex);
             }
-        }
-    }
-
-    private async Task RunPostStepHooks(AsyncServiceScope scope, StepExecutionSummary summary)
-    {
-        var hooks = scope.ServiceProvider.GetServices<IPostStepHook>().ToList();
-        if (hooks.Count == 0)
-        {
-            logger.LogDebug("No post-step hooks registered.");
-            return;
-        }
-
-        logger.LogDebug("Running post-step hooks...");
-        foreach (var hook in hooks)
-        {
-            await RunPostStepHook(hook, summary);
-        }
-
-        logger.LogDebug("Post-step hooks complete.");
-    }
-
-    private async Task RunPostStepHook(IPostStepHook hook, StepExecutionSummary summary)
-    {
-        try
-        {
-            await hook.PostStep(summary, CancellationToken.None);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Unhandled error during post-step hook. Continuing...");
         }
     }
 }
