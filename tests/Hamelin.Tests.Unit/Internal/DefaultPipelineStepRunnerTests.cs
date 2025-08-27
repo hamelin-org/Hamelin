ed3be3518bca865748bd5272546d6747aedd01cd
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-27 11:48:49 +0100

    test: more tests

--- tests/Hamelin.Tests.Unit/Internal/DefaultPipelineStepRunnerTests.cs
+++ tests/Hamelin.Tests.Unit/Internal/DefaultPipelineStepRunnerTests.cs
@@ -52,10 +52,72 @@ public class DefaultPipelineStepRunnerTests
     }
 
     [Fact]
+    public async Task RunStep_WithPreStepHookErrors_StillRunsHooks()
+    {
+        // Arrange
+        var hook1 = Substitute.For<IPreStepHook>();
+        hook1
+            .PreStep(Arg.Any<CancellationToken>())
+            .ThrowsAsync<Exception>();
+
+        var hook2 = Substitute.For<IPreStepHook>();
+        var step = Substitute.For<IPipelineStep>();
+        var sut = DefaultPipelineStepRunnerHelpers.CreateRunner();
+
+        var scope = new ServiceCollection()
+            .AddSingleton(hook1)
+            .AddSingleton(hook2)
+            .BuildServiceProvider()
+            .CreateAsyncScope();
+
+        // Act
+        await sut.RunStep(scope, step, CancellationToken.None);
+
+        // Assert
+        Received.InOrder(() =>
+        {
+            hook1.PreStep(Arg.Any<CancellationToken>());
+            hook2.PreStep(Arg.Any<CancellationToken>());
+            step.Run(Arg.Any<CancellationToken>());
+        });
+    }
+
+    [Fact]
     public async Task RunStep_WithPostStepHooks_RunsHooks()
     {
         // Arrange
         var hook1 = Substitute.For<IPostStepHook>();
+        var hook2 = Substitute.For<IPostStepHook>();
+        var step = Substitute.For<IPipelineStep>();
+        var sut = DefaultPipelineStepRunnerHelpers.CreateRunner();
+
+        var scope = new ServiceCollection()
+            .AddSingleton(hook1)
+            .AddSingleton(hook2)
+            .BuildServiceProvider()
+            .CreateAsyncScope();
+
+        // Act
+        await sut.RunStep(scope, step, CancellationToken.None);
+
+        // Assert
+        Received.InOrder(() =>
+        {
+            step.Run(Arg.Any<CancellationToken>());
+            hook1.PostStep(Arg.Any<StepExecutionSummary>(), Arg.Any<CancellationToken>());
+            hook2.PostStep(Arg.Any<StepExecutionSummary>(), Arg.Any<CancellationToken>());
+        });
+    }
+
+    [Fact]
+    public async Task RunStep_WithPostStepHookException_StillRunsHooks()
+    {
+        // Arrange
+        var hook1 = Substitute.For<IPostStepHook>();
+        hook1
+            .PostStep(Arg.Any<StepExecutionSummary>(), Arg.Any<CancellationToken>())
+            .ThrowsAsync<Exception>();
+
         var hook2 = Substitute.For<IPostStepHook>();
         var step = Substitute.For<IPipelineStep>();
         var sut = DefaultPipelineStepRunnerHelpers.CreateRunner();
