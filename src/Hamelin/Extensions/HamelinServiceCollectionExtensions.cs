4648800c8596c5d346ca9ad7b9f1fe3a61f91572
Tom Wolfe <tom.wolfe@disastercare.co.uk>
2025-11-03 17:15:11 +0000

    feat: add hook utility methods

--- src/Hamelin/Extensions/HamelinServiceCollectionExtensions.cs
+++ src/Hamelin/Extensions/HamelinServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Hamelin.Hooks;
 using Microsoft.Extensions.DependencyInjection;
 
 // ReSharper disable once CheckNamespace
@@ -15,7 +16,8 @@ public static class HamelinServiceCollectionExtensions
     /// <param name="services">The service collection to add the step to.</param>
     /// <typeparam name="TPipelineStep">The type of the pipeline step to register. It must implement <see cref="IPipelineStep"/>.</typeparam>
     /// <returns>The updated service collection with the step registered.</returns>
-    public static IServiceCollection AddStep<TPipelineStep>(this IServiceCollection services) where TPipelineStep : class, IPipelineStep
+    public static IServiceCollection AddStep<TPipelineStep>(this IServiceCollection services)
+        where TPipelineStep : class, IPipelineStep
     {
         return services.AddTransient<TPipelineStep>();
     }
@@ -50,5 +52,92 @@ public static class HamelinServiceCollectionExtensions
     {
         var assembly = typeof(TType).Assembly;
         return services.AddStepsFromAssembly(assembly);
+    }
+
+    /// <summary>
+    /// Registers a pre-pipeline hook in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the hook to.</param>
+    /// <typeparam name="THook">The type of the pre-pipeline hook to register. It must implement <see cref="IPrePipelineHook"/>.</typeparam>
+    /// <returns>The updated service collection with the hook registered.</returns>
+    public static IServiceCollection AddPrePipelineHook<THook>(this IServiceCollection services)
+        where THook : class, IPrePipelineHook
+    {
+        return services.AddTransient<IPrePipelineHook, THook>();
+    }
+
+    /// <summary>
+    /// Registers a post-pipeline hook in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the hook to.</param>
+    /// <typeparam name="THook">The type of the pre-pipeline hook to register. It must implement <see cref="IPostPipelineHook"/>.</typeparam>
+    /// <returns>The updated service collection with the hook registered.</returns>
+    public static IServiceCollection AddPostPipelineHook<THook>(this IServiceCollection services)
+        where THook : class, IPostPipelineHook
+    {
+        return services.AddTransient<IPostPipelineHook, THook>();
+    }
+
+    /// <summary>
+    /// Registers a pre-step hook in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the hook to.</param>
+    /// <typeparam name="THook">The type of the pre-pipeline hook to register. It must implement <see cref="IPreStepHook"/>.</typeparam>
+    /// <returns>The updated service collection with the hook registered.</returns>
+    public static IServiceCollection AddPreStepHook<THook>(this IServiceCollection services)
+        where THook : class, IPreStepHook
+    {
+        return services.AddTransient<IPreStepHook, THook>();
+    }
+
+    /// <summary>
+    /// Registers a post-step hook in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the hook to.</param>
+    /// <typeparam name="THook">The type of the pre-pipeline hook to register. It must implement <see cref="IPostStepHook"/>.</typeparam>
+    /// <returns>The updated service collection with the hook registered.</returns>
+    public static IServiceCollection AddPostStepHook<THook>(this IServiceCollection services)
+        where THook : class, IPostStepHook
+    {
+        return services.AddTransient<IPostStepHook, THook>();
+    }
+
+    /// <summary>
+    /// Registers the hooks contained within the given assembly in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the hooks to.</param>
+    /// <param name="assembly">The assembly from which to add the discovered hooks.</param>
+    /// <returns>The updated service collection with the hooks registered.</returns>
+    public static IServiceCollection AddHooksFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var allTypes = assembly.GetTypes();
+        AddHooksOfType(typeof(IPrePipelineHook));
+        AddHooksOfType(typeof(IPostPipelineHook));
+        AddHooksOfType(typeof(IPreStepHook));
+        AddHooksOfType(typeof(IPostStepHook));
+
+        return services;
+
+        void AddHooksOfType(Type hookType)
+        {
+            var hookTypes = allTypes
+                .Where(t => t is { IsAbstract: false, IsClass: true } && hookType.IsAssignableFrom(t));
+            foreach (var type in hookTypes)
+            {
+                services.AddTransient(hookType, type);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Registers the hooks contained within the assembly that contains the given type.
+    /// </summary>
+    /// <param name="services">The service collection to add the hooks to.</param>
+    /// <typeparam name="TType">The type from which to get the assembly containing the hooks.</typeparam>
+    /// <returns>The updated service collection with the hooks registered.</returns>
+    public static IServiceCollection AddHooksFromAssemblyContaining<TType>(this IServiceCollection services)
+    {
+        var assembly = typeof(TType).Assembly;
+        return services.AddHooksFromAssembly(assembly);
     }
 }
