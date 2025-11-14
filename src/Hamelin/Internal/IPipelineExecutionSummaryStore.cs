62ba5fb34ae4f53f10e992c401d84a4f1916e423
Tom Wolfe <tom.wolfe@disastercare.co.uk>
2025-11-14 15:21:28 +0000

    fix: remove unused classes.

--- src/Hamelin/Internal/IPipelineExecutionSummaryStore.cs
+++
@@ -1,7 +0,0 @@
-namespace Hamelin.Internal;
-
-internal interface IPipelineExecutionSummaryStore
-{
-    public void SetSummary(PipelineExecutionSummary summary);
-    public PipelineExecutionSummary? GetAndClearSummary();
-}
